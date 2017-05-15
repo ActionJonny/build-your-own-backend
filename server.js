bef5aeb684cabcd7a24b63f77233466416f89a6f
Michael Fenwick <mickyfen17@aol.com>
2017-05-15 16:42:05 -0600

    add airbnb linter and fix errors

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var fs = require('fs');
+const fs = require('fs');
 
 const styles = JSON.parse(fs.readFileSync('./writeData/styles.txt').toString())
 const categories = JSON.parse(fs.readFileSync('./writeData/categories.txt').toString());
