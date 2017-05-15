65f0b1104f7ebe36417fd71d5b60f1b284565b88
Michael Fenwick <mickyfen17@aol.com>
2017-05-15 15:52:27 -0600

    add functionality to scrape csv and write to txt

---
+++ scrapeData.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const csv = require('csvtojson');
+
+const beerStylesFile = './csv/styles.csv';
+const beerCategoriesFile = './csv/categories.csv';
+
+csv().fromFile(beerStylesFile)
+  .on('end_parsed', jsonArrObj => {
+    return writeToFile('./writeData/styles.txt', jsonArrObj)
+  })
+  .on('error', err => {
+    console.log('An Error Has Occured');
+    console.log(err);
+  })
+  .on('done', () => {
+    console.log('Scraping Complete');
+  })
+
+csv().fromFile(beerCategoriesFile)
+  .on('end_parsed', jsonArrObj => {
+    return writeToFile('./writeData/categories.txt', jsonArrObj)
+  })
+  .on('error', err => {
+    console.log('An Error Has Occured');
+    console.log(err);
+  })
+  .on('done', () => {
+    console.log('Scraping Complete');
+  })
+
+const writeToFile = (path, data) => {
+  fs.writeFile(path, JSON.stringify(data, null, 2), (err) => {
+    if(err) {
+      console.error('Error:', err.message);
+    } else {
+      console.log('Successful Write to ./test.txt');
+    }
+  })
+}
