a69e7a80e127d6d43338e60a3b22d2e62e3dab32
Michael Fenwick <mickyfen17@aol.com>
2017-05-15 15:53:13 -0600

    add writeData for txts and test parse within server.js

---
+++ server.js
@@ -0,0 +1,4 @@
+var fs = require('fs');
+
+const styles = JSON.parse(fs.readFileSync('./writeData/styles.txt').toString())
+const categories = JSON.parse(fs.readFileSync('./writeData/categories.txt').toString());
