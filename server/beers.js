3c1478ae1ca5d921368499d917cc36849463468f
Michael Fenwick <mickyfen17@aol.com>
2017-05-17 17:22:39 -0600

    add query GET for beer category

--- server/beers.js
+++ server/beers.js
@@ -6,13 +6,32 @@ const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 
 beers.get('/beers', (request, response) => {
-  database('beers').select()
-    .then((beers) => {
-      response.status(200).json(beers);
+  const { category } = request.query;
+  if(!category) {
+    database('beers').select()
+    .then((allBeers) => {
+      response.status(200).json(allBeers);
     })
     .catch((error) => {
       response.status(500).send({ error });
     });
+  } else {
+    database('categories').where(database.raw('LOWER("name")'), category.toLowerCase())
+    .then((beerCategory) => {
+      if (!beerCategory.length) {
+        response.status(404).send({ error: 'No beers found for that category' });
+      } else {
+        const { id } = beerCategory[0];
+        database('beers').where('cat_id', id).select()
+        .then((beerList) => {
+          response.status(200).json(beerList);
+        });
+      }
+    })
+    .catch((error) => {
+      response.status(500).send({ error });
+    });
+  }
 });
 
 beers.get('/beers/:id', (request, response) => {
@@ -29,5 +48,6 @@ beers.get('/beers/:id', (request, response) => {
       response.status(500).send({ error });
     });
 });
+
 
 module.exports = beers;
