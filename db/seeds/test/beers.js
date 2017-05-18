337745c04b77f3c544818b17b117f1fc05b0136c
Michael Fenwick <mickyfen17@aol.com>
2017-05-17 22:42:16 -0600

    alter seed files for altered schemas

--- db/seeds/test/beers.js
+++ db/seeds/test/beers.js
@@ -8,14 +8,21 @@ const breweries = JSON.parse(fs.readFileSync('./writeData/breweries.txt').toStri
 const beerCategories = (knex) => {
   return categories.map((category) => {
     const { id, cat_name } = category;
-    return knex('categories').insert({ id, name: cat_name });
+    return knex('categories').insert({ category_id: id, name: cat_name });
   });
 };
 
 const beerStyles = (knex) => {
   return styles.map((style) => {
     const { id, cat_id, style_name } = style;
-    return knex('styles').insert({ id, name: style_name, category_id: parseInt(cat_id, 10) });
+    return knex('styles').insert({ style_id: id, name: style_name, category_id: parseInt(cat_id, 10) });
+  });
+};
+
+const breweriesJSON = (knex) => {
+  return breweries.map((brewery) => {
+    const { id, name, address1, city, state, code, country } = brewery;
+    return knex('breweries').insert({ brewery_id: id, name, address1, city, state, code, country });
   });
 };
 
@@ -24,16 +31,10 @@ const beersJSON = (knex) => {
     const { id, name, brewery_id } = beer;
     const style_id = beer.style_id === null ? beer.style_id : parseInt(beer.style_id, 10);
     const cat_id = beer.cat_id === null ? beer.cat_id : parseInt(beer.cat_id, 10);
-    return knex('beers').insert({ id, name, cat_id, style_id, brewery_id: parseInt(brewery_id, 10) });
+    return knex('beers').insert({ beer_id: id, name, cat_id, style_id, brewery_id: parseInt(brewery_id, 10) });
   });
 };
 
-const breweriesJSON = (knex) => {
-  return breweries.map((brewery) => {
-    const { id, name, address1, city, state, code, country } = brewery;
-    return knex('breweries').insert({ id, name, address1, city, state, code, country });
-  });
-};
 
 exports.seed = (knex, Promise) => {
   return knex('styles').del()
@@ -43,8 +44,8 @@ exports.seed = (knex, Promise) => {
     .then(() => {
       const beerCategory = beerCategories(knex);
       const beerStyle = beerStyles(knex);
-      const beersArray = beersJSON(knex);
       const breweriesArray = breweriesJSON(knex);
+      const beersArray = beersJSON(knex);
       return Promise.all([...beerCategory, ...beerStyle, ...breweriesArray, ...beersArray]);
     });
 };
