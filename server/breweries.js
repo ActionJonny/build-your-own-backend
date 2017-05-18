51388ae55507b181b6c7437d858bd9b9737238b1
Michael Fenwick <mickyfen17@aol.com>
2017-05-18 17:57:44 -0600

    add PATCH req for breweries

--- server/breweries.js
+++ server/breweries.js
@@ -66,4 +66,22 @@ breweries.post('/breweries', (request, response) => {
     });
 });
 
+breweries.patch('/breweries/:id', (request, response) => {
+  const { id } = request.params;
+  const expectedRequest = ['name', 'address1', 'city', 'state', 'code', 'country'];
+  const isMissing = expectedRequest.some(param => request.body[param]);
+  let brewery = request.body;
+
+  if (!isMissing) { return response.status(422).send({ error: 'Missing content from patch' }); }
+
+    database('breweries').where('brewery_id', id)
+    .update(brewery, ['name', 'address1', 'city', 'state', 'code', 'country'])
+      .then((updatedBrewery) => {
+        response.status(204).send(...updatedBrewery);
+      })
+      .catch((error) => {
+        response.status(500).send({ error });
+      });
+});
+
 module.exports = breweries;
